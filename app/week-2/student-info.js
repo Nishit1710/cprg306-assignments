f53391687dfcf5de4a6e5a19eeeef72b74bd38c7
Nishit1710 <nishitpatel10481@gmail.com>
2024-01-25 12:31:02 -0700

    Edited root page.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Nishit Patel</p>
+      <Link href="https://github.com/Nishit1710/cprg306-assignments">My GitHub Repository</Link>
+    </div>
+  );
+};
