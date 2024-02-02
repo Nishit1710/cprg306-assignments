75e5dc952fb3f8debbb94db12d4a984606d50790
Nishit1710 <nishitpatel10481@gmail.com>
2024-02-02 15:01:45 -0700

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,9 +3,9 @@ import ItemList from "./item-list.js";
 export default function Page() {
     return (
       <main>
-        <div class= "flex justify-center">
-        <div class="py-8"></div>
-        <p class="text-5xl">
+        <div className= "flex justify-center">
+        <div className="py-8"></div>
+        <p className="text-5xl">
         <div>
         <h1>Shopping List</h1>
         </div></p></div>
